ad63a347c48b05ef7745db6717b4e690d8531c08
Darisset <111202013179@mhs.dinus.ac.id>
2023-01-06 22:12:02 +0700

    update authentication webpack loader

---
+++ static/authentication/index.js
@@ -0,0 +1,14 @@
+
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'metismenu/dist/metisMenu.min.js';
+import 'simplebar/dist/simplebar.min.js';
+import 'node-waves/dist/waves.min.js';
+
+import '../css/bootstrap.min.css';
+import '../css/icons.min.css';
+import '../css/app.min.css';
+
+
+
+window.jQuery = window.$ = require('jquery');
+
